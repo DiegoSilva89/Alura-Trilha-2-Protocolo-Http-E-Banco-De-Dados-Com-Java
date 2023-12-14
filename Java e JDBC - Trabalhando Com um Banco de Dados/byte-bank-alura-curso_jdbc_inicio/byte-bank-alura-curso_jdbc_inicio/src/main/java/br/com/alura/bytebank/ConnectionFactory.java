5cd4d59dd79ff06cbb8d47e21c43d0c040589f76
DiegoSilva89 <diegosantosdasilva89@gmail.com>
2023-12-14 08:06:14 -0300

    salvando conta no banco

--- Java e JDBC - Trabalhando Com um Banco de Dados/byte-bank-alura-curso_jdbc_inicio/byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ Java e JDBC - Trabalhando Com um Banco de Dados/byte-bank-alura-curso_jdbc_inicio/byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -5,11 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-
+/*Os padrões factory têm como principal objetivo nos auxiliar a reduzir acoplamento em nosso software, ou seja,
+manter dependências flexíveis*/
     public Connection recuperarConexao() {
         try {
             return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3386/byte_bank?user=root&password=root");
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=1007");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
