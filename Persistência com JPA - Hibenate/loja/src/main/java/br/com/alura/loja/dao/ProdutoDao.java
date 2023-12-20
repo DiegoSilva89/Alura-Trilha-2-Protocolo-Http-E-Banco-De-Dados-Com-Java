939f55168bf579414c0cb36e7d75072ccb16c7a3
DiegoSilva89 <diegosantosdasilva89@gmail.com>
2023-12-20 06:45:53 -0300

    Finalizando projeto

--- Persistência com JPA - Hibenate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ Persistência com JPA - Hibenate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class ProdutoDao {
 
     private EntityManager em;
@@ -23,6 +26,36 @@ public class ProdutoDao {
     public void remover(Produto produto) {
         produto = em.merge(produto);
         this.em.remove(produto);
+    }
+
+    public Produto buscarPorId (Long id) {
+        return em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "SELECT p FROM Produto p";
+        return em.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeDaCategoria(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();
     }
 
 }
